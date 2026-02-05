553ebfaf0e7ff48eedf6d72fb45775d2fffc7fae
akhildp <akhildp@gmail.com>
2026-02-05 15:58:18 -0500

    add site admin editing

---
+++ docs/js/netlify-setup.js
@@ -0,0 +1,4 @@
+// Set global options for Netlify Identity to point to the external Netlify site
+window.netlifyIdentityOptions = {
+    APIUrl: "https://heartfelt-axolotl-2d5788.netlify.app/.netlify/identity"
+};
